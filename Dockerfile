eb7a0b08b2f09b85801157ae9313ee08bbce3a60
Bruno Querino <bquerino@gmail.com>
2021-04-04 02:47:04 -0300

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM adoptopenjdk/openjdk11:alpine
+VOLUME /tmp
+ARG REGION_ARG=sa-east-1
+ARG ACCESS_ARG
+ARG SECRET_ARG
+ENV AWS_REGION=$REGION_ARG
+ENV AWS_ACCESS_KEY=$ACCESS_ARG
+ENV AWS_SECRET_KEY=$SECRET_ARG
+ARG JAR_FILE=build/libs/*.jar
+COPY ${JAR_FILE} app.jar
+ENTRYPOINT ["java","-jar","/app.jar"]
---
+++ src/main/kotlin/com/mycompany/helloworld/HelloworldApplication.kt
@@ -0,0 +1,25 @@
+package com.mycompany.helloworld
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@SpringBootApplication
+class HelloworldApplication
+
+fun main(args: Array<String>) {
+	runApplication<HelloworldApplication>(*args)
+}
+
+@RestController
+class MessageResource {
+	@GetMapping
+	fun index(): List<Message> = listOf(
+		Message("1", "Hello world!"),
+		Message("2", "Bonjour le monde!"),
+		Message("3", "Olá mundo!"),
+	)
+}
+
+data class Message(val id: String?, val text: String)
